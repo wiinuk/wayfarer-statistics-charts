4dcda7e9885d5c67f4b6554289b3d135f0319bda
wiinuk <wii.nukegara@gmail.com>
2026-08-26 17:28:46 +0900

    0.3.1

--- wayfarer-statistics-charts.user.js
+++ wayfarer-statistics-charts.user.js
@@ -5,11 +5,11 @@
 // @downloadURL  https://github.com/wiinuk/wayfarer-statistics-charts/raw/main/wayfarer-statistics-charts.user.js
 // @updateURL    https://github.com/wiinuk/wayfarer-statistics-charts/raw/main/wayfarer-statistics-charts.user.js
 // @homepageURL  https://github.com/wiinuk/wayfarer-statistics-charts
-// @version      0.2.2
+// @version      0.3.1
 // @description  Visualize statistics of Niantic Wayfarer submissions.
 // @author       Wiinuk
-// @match        https://wayfarer.nianticlabs.com/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=nianticlabs.com
+// @match        https://wayfarer.scopely.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=scopely.com
 // ==/UserScript==
 
 /******/ (() => { // webpackBootstrap
